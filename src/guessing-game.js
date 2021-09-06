ed1021b0a85a04ed77d6c9bfeda832896144c7fe
manol1 <marsen1491@gmail.com>
2021-09-06 12:03:39 +0300

    feat: ok

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.left = null;
+    this.right = null;
+    this.result = null;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.left = min;
+    this.right = max;
+  }
 
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  guess() {
+    this.result = Math.round((this.left + this.right) / 2);
+    return this.result;
+  }
+  
+  lower() {
+    this.right = this.result;
+  }
+    
+  greater() {
+    this.left = this.result;
+  }
 }
 
 module.exports = GuessingGame;
